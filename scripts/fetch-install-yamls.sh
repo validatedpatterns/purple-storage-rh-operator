80daacc54e59ec5038855db835d2df0718eb1957
Michele Baldessari <michele@acksyn.org>
2025-02-18 12:11:26 +0100

    Add yaml fetching script

---
+++ scripts/fetch-install-yamls.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+VERSIONS=("v5.2.2.0" "v5.2.1.1")
+
+RAW_FILE_URL="https://raw.githubusercontent.com/IBM/ibm-spectrum-scale-container-native"
+
+for VERSION in "${VERSIONS[@]}"; do
+    echo "Processing branch: $VERSION"
+    mkdir -p "files/$VERSION"
+    
+    FILE_URL="$RAW_FILE_URL/$VERSION/generated/scale/install.yaml"
+    curl -L -o "files/$VERSION/install.yaml" "$FILE_URL"
+    
+    if [[ $? -eq 0 ]]; then
+        echo "Downloaded install.yaml for $VERSION"
+    else
+        echo "Failed to download install.yaml for $VERSION"
+    fi
+done
